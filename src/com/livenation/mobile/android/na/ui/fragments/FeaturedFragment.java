0aa4f787fcca0bef659fe981583c6e4a6064822c
Charlie Chilton <charliec@livenationlabs.com>
2014-01-17 11:49:38 -0800

    Feature carousel looks prettier

--- src/com/livenation/mobile/android/na/ui/fragments/FeaturedFragment.java
+++ src/com/livenation/mobile/android/na/ui/fragments/FeaturedFragment.java
@@ -15,8 +15,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.livenation.mobile.android.na.R;
 import com.livenation.mobile.android.na.app.LiveNationApplication;
 import com.livenation.mobile.android.na.helpers.LocationHelper;
@@ -25,9 +27,9 @@ import com.livenation.mobile.android.platform.api.service.livenation.impl.model.
 import com.livenation.mobile.android.platform.api.service.livenation.impl.parameter.ApiParameters;
 
 public class FeaturedFragment extends Fragment {
-	private ViewGroup chartingContainer; 
+	private ViewGroup chartingContainer;
 	private LocationCallback locationCallback = null;
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -36,29 +38,29 @@ public class FeaturedFragment extends Fragment {
 			getLocationHelper().getLocation(getActivity(), locationCallback);
 		}
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View result=inflater.inflate(R.layout.fragment_featured, container, false);
+		View result = inflater.inflate(R.layout.fragment_featured, container, false);
 		chartingContainer = (ViewGroup) result.findViewById(R.id.featured_charting_container);
-		return(result);
+		return result;
 	}
-	
+
 	private void getData(double lat, double lng) {
 		ApiParameters.TopChartParameters parameters = ApiParameters.createChartParameters();
 		parameters.setLocation(lat, lng);
 		getApiService().getTopCharts(parameters, new OnGetTopCharts());
 	}
-	
+
 	private LiveNationApiService getApiService() {
 		return LiveNationApplication.get().getServiceApi();
 	}
-	
+
 	private LocationHelper getLocationHelper() {
 		return LiveNationApplication.get().getLocationHelper();
 	}
-	
+
 	private class LocationCallback implements LocationHelper.LocationCallback {
 
 		@Override
@@ -68,26 +70,32 @@ public class FeaturedFragment extends Fragment {
 
 		@Override
 		public void onLocationFailure(int failureCode) {
-			
+
 		}
 	};
-	
-	private class OnGetTopCharts implements LiveNationApiService.GetTopChartsCallback {
+
+	private class OnGetTopCharts implements
+			LiveNationApiService.GetTopChartsCallback {
 
 		@Override
 		public void onGetCharts(List<Chart> topCharts) {
 			for (Chart chart : topCharts) {
-				TextView view = new TextView(getActivity());
-				view.setText(chart.getTitle());
-				chartingContainer.addView(view);
+				NetworkImageView image = new NetworkImageView(getActivity());
+				image.setImageUrl(chart.getFeatureImageUrl(), getImageLoader());
+				chartingContainer.addView(image, new LayoutParams(
+						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 			}
 		}
 
 		@Override
 		public void onFailure(int arg0, String arg1) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
+	}
+
+	private ImageLoader getImageLoader() {
+		return LiveNationApplication.get().getImageLoader();
 	}
 }
