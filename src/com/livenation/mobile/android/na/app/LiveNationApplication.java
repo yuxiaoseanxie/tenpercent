65ce32245431258cef7f4c87ee59e5545fa5f316
Charlie Chilton <charliec@livenationlabs.com>
2014-01-17 11:17:08 -0800

    Adding memory based LRU cache for images

--- src/com/livenation/mobile/android/na/app/LiveNationApplication.java
+++ src/com/livenation/mobile/android/na/app/LiveNationApplication.java
@@ -10,15 +10,20 @@ package com.livenation.mobile.android.na.app;
 
 import android.app.Application;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 import com.livenation.mobile.android.na.helpers.LocationHelper;
 import com.livenation.mobile.android.platform.api.service.livenation.LiveNationApiService;
 import com.livenation.mobile.android.platform.api.service.livenation.impl.LiveNationApiServiceImpl;
 
 public class LiveNationApplication extends Application {
 	private static LiveNationApplication instance;
-	private static LiveNationApiService serviceApi;
-	private static LocationHelper locationHelper;
-	
+	private LiveNationApiService serviceApi;
+	private LocationHelper locationHelper;
+	private ImageLoader imageLoader;
+	private RequestQueue requestQueue;
+
 	public static LiveNationApplication get() {
 		return instance;
 	}
@@ -30,6 +35,10 @@ public class LiveNationApplication extends Application {
 		serviceApi = new LiveNationApiServiceImpl(Constants.clientId, Constants.deviceId, getApplicationContext());
 		locationHelper = new LocationHelper();
 		locationHelper.prepareCache(getApplicationContext());
+		requestQueue = Volley.newRequestQueue(getApplicationContext());
+		int defaultCacheSize = MemoryImageCache.getDefaultLruSize();
+		MemoryImageCache cache = new MemoryImageCache(defaultCacheSize);
+		imageLoader = new ImageLoader(requestQueue, cache);
 	}
 	
 	public LiveNationApiService getServiceApi() {
@@ -38,5 +47,9 @@ public class LiveNationApplication extends Application {
 	
 	public LocationHelper getLocationHelper() {
 		return locationHelper;
+	}
+	
+	public ImageLoader getImageLoader() {
+		return imageLoader;
 	}
 }
