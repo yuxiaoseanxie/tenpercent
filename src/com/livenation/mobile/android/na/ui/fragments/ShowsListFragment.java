ec42919d5eeae95280a05ad3fbfd22f039c2158d
Charlie Chilton <charliec@livenationlabs.com>
2014-01-17 17:25:45 -0800

    Copyright headers

--- src/com/livenation/mobile/android/na/ui/fragments/ShowsListFragment.java
+++ src/com/livenation/mobile/android/na/ui/fragments/ShowsListFragment.java
@@ -1,3 +1,11 @@
+/*
+ * 
+ * @author Charlie Chilton 2014/01/16
+ * 
+ * Copyright (C) 2014 Live Nation Labs. All rights reserved.
+ * 
+ */
+
 package com.livenation.mobile.android.na.ui.fragments;
 
 import java.text.ParseException;
--- src/com/livenation/mobile/android/na/ui/fragments/support/LiveNationListFragment.java
+++ src/com/livenation/mobile/android/na/ui/fragments/support/LiveNationListFragment.java
@@ -1,3 +1,11 @@
+/*
+ * 
+ * @author Charlie Chilton 2014/01/17
+ * 
+ * Copyright (C) 2014 Live Nation Labs. All rights reserved.
+ * 
+ */
+
 package com.livenation.mobile.android.na.ui.fragments.support;
 
 import com.livenation.mobile.android.na.app.LiveNationApplication;
