62cceeeaccd8926afc7d535d3a64f064252a5196
Charlie Chilton <charliec@livenationlabs.com>
2014-02-05 10:14:20 -0800

    Fragments: Normalized TabHostFragment into parent activity

--- src/main/com/livenation/mobile/android/na/ui/LandingActivity.java
+++ src/main/com/livenation/mobile/android/na/ui/LandingActivity.java
@@ -12,14 +12,19 @@ import android.app.ActionBar;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
+import android.widget.TabHost.TabSpec;
 
 import com.livenation.mobile.android.na.R;
+import com.livenation.mobile.android.na.ui.fragments.AllShowsFragment;
 
 public class LandingActivity extends FragmentActivity {
 	private ActionBarDrawerToggle drawerToggle;
+	private FragmentTabHost tabHost;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,31 @@ public class LandingActivity extends FragmentActivity {
 											R.string.actionbar_drawer_open,
 											R.string.actionbar_drawer_close);
 		rootView.setDrawerListener(drawerToggle);
+		tabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+	    
+		tabHost.setup(this, getSupportFragmentManager(),
+				R.id.activity_landing_container);
 		
-
+		String title = "";
+		TabSpec tabSpec;
+		
+		title = getString(R.string.tab_title_all_shows);
+		tabSpec = tabHost.newTabSpec("all_shows");
+		tabSpec.setIndicator(title);
+		tabHost.addTab(tabSpec,
+				AllShowsFragment.class, null);
+		
+		title = getString(R.string.tab_title_nearby);
+		tabSpec = tabHost.newTabSpec("nearby");
+		tabSpec.setIndicator(title);
+		tabHost.addTab(tabSpec,
+				Fragment.class, null);
+		
+		title = getString(R.string.tab_title_your_shows);
+		tabSpec = tabHost.newTabSpec("your_shows");
+		tabSpec.setIndicator(title);
+		tabHost.addTab(tabSpec,
+				Fragment.class, null);
 	}
 	
 	@Override
