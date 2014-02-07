45c826a33c339802f966b707b8ba9b51e862f029
Charlie Chilton <charliec@livenationlabs.com>
2014-02-07 13:10:05 -0800

    UI: Styling TabHost tabs to LiveNation red

--- src/main/com/livenation/mobile/android/na/ui/LandingActivity.java
+++ src/main/com/livenation/mobile/android/na/ui/LandingActivity.java
@@ -9,6 +9,7 @@
 package com.livenation.mobile.android.na.ui;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -16,8 +17,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 import com.livenation.mobile.android.na.R;
 import com.livenation.mobile.android.na.ui.fragments.AllShowsFragment;
@@ -46,26 +50,29 @@ public class LandingActivity extends FragmentActivity {
 		tabHost.setup(this, getSupportFragmentManager(),
 				R.id.activity_landing_container);
 		
-		String title = "";
+		String title;
+		View view;
 		TabSpec tabSpec;
 		
 		title = getString(R.string.tab_title_all_shows);
+		view = createTab(LandingActivity.this, title);
 		tabSpec = tabHost.newTabSpec("all_shows");
-		tabSpec.setIndicator(title);
+		tabSpec.setIndicator(view);
 		tabHost.addTab(tabSpec,
 				AllShowsFragment.class, null);
 		
 		title = getString(R.string.tab_title_nearby);
+		view = createTab(LandingActivity.this, title);
 		tabSpec = tabHost.newTabSpec("nearby");
-		tabSpec.setIndicator(title);
+		tabSpec.setIndicator(view);
 		tabHost.addTab(tabSpec,
 				Fragment.class, null);
 		
 		title = getString(R.string.tab_title_your_shows);
+		view = createTab(LandingActivity.this, title);
 		tabSpec = tabHost.newTabSpec("your_shows");
-		tabSpec.setIndicator(title);
-		tabHost.addTab(tabSpec,
-				Fragment.class, null);
+		tabSpec.setIndicator(view);
+		tabHost.addTab(tabSpec, Fragment.class, null);
 	}
 	
 	@Override
@@ -86,6 +93,19 @@ public class LandingActivity extends FragmentActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	/**
+	 * Here we have to return our own Tab View object to get our desired LiveNation red tab.
+	 * 
+	 * Because Google forgot to make the default tabs in the TabHost XML stylable....
+	 * 
+	 */
+	private View createTab(Context context, String title) {
+		View view = LayoutInflater.from(context).inflate(R.layout.view_tab, null);
+		TextView text = (TextView) view.findViewWithTag("titleText");
+		text.setText(title);
+		return view;
 	}
 	
 }
