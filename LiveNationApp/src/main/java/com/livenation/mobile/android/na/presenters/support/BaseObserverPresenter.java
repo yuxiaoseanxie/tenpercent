cdcaa05dd810f79ac68825ec82e0494b2cdc3c8a
Charlie Chilton <charliec@livenationlabs.com>
2014-02-28 10:57:42 -0800

    Cleanup: Removing unnecessary Type

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/presenters/support/BaseObserverPresenter.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/presenters/support/BaseObserverPresenter.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Created by cchilton on 2/27/14.
  */
-public abstract class BaseObserverPresenter<T extends IdEquals, T2, P extends PresenterView, P2 extends BaseState> extends BasePresenter<P, P2> {
+public abstract class BaseObserverPresenter<T extends IdEquals, P extends PresenterView, P2 extends BaseState> extends BasePresenter<P, P2> {
     public abstract void post(T object);
     public abstract void postAll(List<T> object);
     public abstract boolean contains(T object);
