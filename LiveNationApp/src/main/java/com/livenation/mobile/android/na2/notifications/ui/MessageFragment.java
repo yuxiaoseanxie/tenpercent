9fddd2f3172e9deffc9ea8506831126ae5143cac
Kevin MacWhinnie <kevin@pinnaplayer.com>
2014-03-03 14:46:09 -0800

    added subject and date received to MessageFragment [closes #66694296, #66694222]

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na2/notifications/ui/MessageFragment.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na2/notifications/ui/MessageFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.livenation.mobile.android.na2.R;
 import com.urbanairship.Logger;
@@ -18,17 +19,25 @@ import com.urbanairship.richpush.RichPushManager;
 import com.urbanairship.richpush.RichPushMessage;
 import com.urbanairship.widget.RichPushMessageView;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Fragment that displays a rich push activity_message in a RichPushMessageView
  */
 public class MessageFragment extends Fragment {
+    private static final SimpleDateFormat DATE_RECEIVED_FORMAT = new SimpleDateFormat("MMM d, yyyy  h:mm a");
+
     private RichPushMessageView browser;
+    private TextView subjectText;
+    private TextView dateReceivedText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container);
 
         browser = (RichPushMessageView)view.findViewById(R.id.fragment_message_view);
+        subjectText = (TextView)view.findViewById(R.id.fragment_message_subject);
+        dateReceivedText = (TextView)view.findViewById(R.id.fragment_message_date);
 
         return view;
     }
@@ -41,6 +50,9 @@ public class MessageFragment extends Fragment {
         RichPushMessage message = RichPushManager.shared().getRichPushUser().getInbox().getMessage(messageId);
 
         if (message != null) {
+            subjectText.setText(message.getTitle());
+            dateReceivedText.setText(DATE_RECEIVED_FORMAT.format(message.getSentDate()));
+
             browser.loadRichPushMessage(message);
         } else {
             Logger.info("Couldn't retrieve activity_message for ID: " + messageId);
