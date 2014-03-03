dad6967f56cc1059fd88659a3eb1cce97f098505
Kevin MacWhinnie <kevin@pinnaplayer.com>
2014-03-03 11:01:03 -0800

    swapped out clipboard copy code for intent chooser in DebugItem

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na2/ui/support/DebugItem.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na2/ui/support/DebugItem.java
@@ -3,6 +3,7 @@ package com.livenation.mobile.android.na2.ui.support;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 import com.livenation.mobile.android.na2.R;
@@ -13,12 +14,20 @@ import java.io.Serializable;
  * Created by km on 2/28/14.
  */
 public class DebugItem implements Serializable {
+    public static final int TYPE_INFO = 0;
+    public static final int TYPE_ACTION = 1;
+
+
     private String name;
     private String value;
 
     public DebugItem(String name, String value) {
         this.name = name;
-        this.value = value;
+
+        if(value != null)
+            this.value = value;
+        else
+            this.value = "(None)";
     }
 
     public String getName() {
@@ -29,12 +38,17 @@ public class DebugItem implements Serializable {
         return value;
     }
 
+    public int getType() { return TYPE_INFO; }
+
 
     public void doAction(Context context) {
-        ClipboardManager clipboard = (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clipData = ClipData.newPlainText(getName(), getValue());
-        clipboard.setPrimaryClip(clipData);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 
-        Toast.makeText(context, R.string.debug_copied_toast, Toast.LENGTH_SHORT).show();
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, getName());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getValue());
+
+        context.startActivity(Intent.createChooser(shareIntent, "Share debug item"));
     }
 }
