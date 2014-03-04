19fa14c25962007f72bd0adfc29bc39921cd379b
Kevin MacWhinnie <kevin@pinnaplayer.com>
2014-03-03 16:04:33 -0800

    added call to action button to MessageFragment [closes #66694562]

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na2/notifications/ui/MessageFragment.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na2/notifications/ui/MessageFragment.java
@@ -5,15 +5,19 @@
 package com.livenation.mobile.android.na2.notifications.ui;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.livenation.mobile.android.na2.R;
+import com.livenation.mobile.android.na2.app.Constants;
 import com.urbanairship.Logger;
 import com.urbanairship.richpush.RichPushManager;
 import com.urbanairship.richpush.RichPushMessage;
@@ -27,9 +31,11 @@ import java.text.SimpleDateFormat;
 public class MessageFragment extends Fragment {
     private static final SimpleDateFormat DATE_RECEIVED_FORMAT = new SimpleDateFormat("MMM d, yyyy  h:mm a");
 
+    private RichPushMessage message;
     private RichPushMessageView browser;
     private TextView subjectText;
     private TextView dateReceivedText;
+    private Button callToActionButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,6 +44,8 @@ public class MessageFragment extends Fragment {
         browser = (RichPushMessageView)view.findViewById(R.id.fragment_message_view);
         subjectText = (TextView)view.findViewById(R.id.fragment_message_subject);
         dateReceivedText = (TextView)view.findViewById(R.id.fragment_message_date);
+        callToActionButton = (Button)view.findViewById(R.id.fragment_message_cta_button);
+        callToActionButton.setOnClickListener(new CallToActionClickListener());
 
         return view;
     }
@@ -47,11 +55,20 @@ public class MessageFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         String messageId = getActivity().getIntent().getStringExtra(MessageActivity.EXTRA_MESSAGE_ID_KEY);
-        RichPushMessage message = RichPushManager.shared().getRichPushUser().getInbox().getMessage(messageId);
+        message = RichPushManager.shared().getRichPushUser().getInbox().getMessage(messageId);
 
         if (message != null) {
             subjectText.setText(message.getTitle());
             dateReceivedText.setText(DATE_RECEIVED_FORMAT.format(message.getSentDate()));
+
+            Bundle messageExtras = message.getExtras();
+            if(messageExtras.containsKey(Constants.Notifications.EXTRA_MESSAGE_ACTION_URL)) {
+                String title = messageExtras.getString(Constants.Notifications.EXTRA_MESSAGE_ACTION_NAME,
+                                                       getString(R.string.message_cta_default));
+                callToActionButton.setText(title);
+            } else {
+                callToActionButton.setVisibility(View.GONE);
+            }
 
             browser.loadRichPushMessage(message);
         } else {
@@ -76,6 +93,22 @@ public class MessageFragment extends Fragment {
 
         if (Build.VERSION.SDK_INT >= 11) {
             browser.onResume();
+        }
+    }
+
+
+    private class CallToActionClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Bundle messageExtras = message.getExtras();
+            if(!messageExtras.containsKey(Constants.Notifications.EXTRA_MESSAGE_ACTION_URL)) {
+                throw new IllegalStateException("CallToActionClickListener.onClick should never be called without an EXTRA_MESSAGE_ACTION_URL");
+            }
+
+            String url = messageExtras.getString(Constants.Notifications.EXTRA_MESSAGE_ACTION_URL);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
         }
     }
 }
