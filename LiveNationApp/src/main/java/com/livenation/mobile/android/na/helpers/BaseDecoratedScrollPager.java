cf925acfea2c23a0ea005326c7cb310a4d0c0dbc
Charlie Chilton <charliec@livenationlabs.com>
2014-03-17 13:02:52 -0700

    Fix empty whitespace "android bug" left by listview's footer view when it's dismissed

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/BaseDecoratedScrollPager.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/BaseDecoratedScrollPager.java
@@ -3,7 +3,9 @@ package com.livenation.mobile.android.na.helpers;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 
 import com.livenation.mobile.android.na.R;
 import com.livenation.mobile.android.platform.api.service.livenation.helpers.IdEquals;
@@ -17,8 +19,17 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
  * Created by cchilton on 3/12/14.
  */
 public abstract class BaseDecoratedScrollPager<TItemType extends IdEquals<TItemType>> extends BaseScrollPager<TItemType> {
-    private View listLoadingView;
     private List<? extends TItemType> lastFetch;
+    private final View listLoadingView;
+    /*
+    Use a frame layout to contain our loading view. This is necessary since Android doesn't like direct
+    modification to the footview's view, and will leave its whitespace visible even if the view is
+    set to View.GONE :-/
+    http://stackoverflow.com/questions/4317778/hide-footer-view-in-listview?rq=1
+    http://stackoverflow.com/questions/7576099/hiding-footer-in-listview
+     */
+    private final ViewGroup footerBugHack;
+    private final FrameLayout.LayoutParams footerParams;
 
     protected BaseDecoratedScrollPager(int limit, ArrayAdapter<TItemType> adapter) {
         super(limit, adapter);
@@ -26,11 +37,13 @@ public abstract class BaseDecoratedScrollPager<TItemType extends IdEquals<TItemT
         Context context = adapter.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         listLoadingView = inflater.inflate(R.layout.view_loading, null);
+        footerParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        footerBugHack = new FrameLayout(context);
     }
 
     public void setupListView(StickyListHeadersListView listView) {
         listView.setOnScrollListener(this);
-        listView.addFooterView(listLoadingView);
+        listView.addFooterView(footerBugHack);
     }
 
     @Override
@@ -55,12 +68,12 @@ public abstract class BaseDecoratedScrollPager<TItemType extends IdEquals<TItemT
 
     @Override
     public void onFetchStarted() {
-        listLoadingView.setVisibility(View.VISIBLE);
+        footerBugHack.addView(listLoadingView);
     }
 
     @Override
     public void onFetchEnded() {
-        listLoadingView.setVisibility(View.GONE);
+        footerBugHack.removeAllViews();
     }
 
     public abstract void stop();
