b1b09b9d23bf2f81bf16d73004efee287e741b94
Charlie Chilton <charliec@livenationlabs.com>
2014-03-31 14:35:52 -0700

    Removing unused constant

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/app/Constants.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/app/Constants.java
@@ -11,7 +11,6 @@ package com.livenation.mobile.android.na.app;
 import java.util.UUID;
 
 public class Constants {
-	public static final String clientId = "9e34bce8-5ea3-11e3-a9e5-5a59eb79d850";
 
     public static enum Environment {
         Production("https://api.livenation.com", "0361b9ba-9381-11e3-bc4c-bad30bc9cabe"),
