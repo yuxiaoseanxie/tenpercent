8425494f605753dc8a8e5937522c156c0a3083aa
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-04-07 12:15:20 -0700

    call affinities api

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/MusicSyncHelper.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/MusicSyncHelper.java
@@ -4,30 +4,36 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.widget.Toast;
 
+import com.livenation.mobile.android.na.app.ApiServiceBinder;
 import com.livenation.mobile.android.na.app.Constants;
+import com.livenation.mobile.android.na.app.LiveNationApplication;
 import com.livenation.mobile.android.platform.api.service.ApiService;
+import com.livenation.mobile.android.platform.api.service.livenation.LiveNationApiService;
 import com.livenation.mobile.android.platform.api.service.livenation.impl.model.MusicLibrary;
+import com.livenation.mobile.android.platform.api.service.livenation.impl.parameter.ApiParameters;
 
 /**
  * Created by elodieferrais on 4/4/14.
  */
-public class MusicSyncHelper {
+public class MusicSyncHelper implements ApiServiceBinder{
+    private MusicLibrary  musicLibrary;
+    private boolean isToastShowable;
+    private Toast successToast;
+    private Toast failToast;
 
     public void syncMusic(Context context) {
-        final boolean isToastShowable = isToastShowable(context);
+        isToastShowable = isToastShowable(context);
         if (isToastShowable) {
             Toast.makeText(context, "Music Scan started", Toast.LENGTH_SHORT).show();
         }
-        final Toast successToast = Toast.makeText(context, "Music Scan done! ", Toast.LENGTH_SHORT);
-        final Toast failToast = Toast.makeText(context, "Music Scan has failed! ", Toast.LENGTH_SHORT);
+        successToast = Toast.makeText(context, "Music Scan done! ", Toast.LENGTH_SHORT);
+        failToast = Toast.makeText(context, "Music Scan has failed! ", Toast.LENGTH_SHORT);
         MusicLibraryScannerHelper musicLibraryScannerHelper = new MusicLibraryScannerHelper();
         musicLibraryScannerHelper.getMusicDiffSinceLastSync(context, new ApiService.BasicApiCallback<MusicLibrary>() {
             @Override
             public void onSuccess(MusicLibrary result) {
-                if (isToastShowable) {
-                    successToast.setText("Music Scan done! " + String.valueOf(result.getData().size()) + " artist has been synchronyzed");
-                    successToast.show();
-                }
+                musicLibrary = result;
+                LiveNationApplication.get().getApiHelper().bindApi(MusicSyncHelper.this);
             }
 
             @Override
@@ -42,5 +48,25 @@ public class MusicSyncHelper {
     private boolean isToastShowable(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.SharedPreferences.DEBUG_MODE_DATA, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(Constants.SharedPreferences.DEBUG_MODE_IS_DEBUG_MODE_ACTIVATED, false);
+    }
+
+    @Override
+    public void onApiServiceAttached(LiveNationApiService apiService) {
+        apiService.sendLibraryAffinities(ApiParameters.createLibraryAffinitiesParameters().setLibraryDump(musicLibrary), new LiveNationApiService.SendLibraryAffinitiesCallback() {
+            @Override
+            public void onSuccess() {
+                if (isToastShowable) {
+                    successToast.setText("Music Scan done! " + String.valueOf(musicLibrary.getData().size()) + " artist has been synchronyzed");
+                    successToast.show();
+                }
+            }
+
+            @Override
+            public void onFailure(int errorCode, String message) {
+                if (isToastShowable) {
+                    failToast.show();
+                }
+            }
+        });
     }
 }
