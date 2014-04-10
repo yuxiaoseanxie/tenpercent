438886955756a3459df0d4ac56f0eebf4fd63c00
Kevin MacWhinnie <kevin@pinnaplayer.com>
2014-04-10 10:56:51 -0700

    bottom line visible option on EventAdapter

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/adapters/EventAdapter.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/adapters/EventAdapter.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class EventAdapter extends ArrayAdapter<Event> {
     private ShowView.DisplayMode displayMode;
+    private boolean bottomLineVisible;
 
     public EventAdapter(Context context, ShowView.DisplayMode displayMode) {
         this(context, displayMode, new ArrayList<Event>());
@@ -22,6 +23,7 @@ public class EventAdapter extends ArrayAdapter<Event> {
         super(context, android.R.layout.simple_list_item_1, events);
 
         this.displayMode = displayMode;
+        this.bottomLineVisible = false;
     }
 
     //region Overrides
@@ -32,7 +34,7 @@ public class EventAdapter extends ArrayAdapter<Event> {
         if(view == null) {
             view = new ShowView(getContext());
             view.setDisplayMode(getDisplayMode());
-            view.setBottomLineVisible(false);
+            view.setBottomLineVisible(isBottomLineVisible());
         }
 
         Event event = getItem(position);
@@ -44,10 +46,18 @@ public class EventAdapter extends ArrayAdapter<Event> {
     //endregion
 
 
-    //region Getters
+    //region Properties
 
     public ShowView.DisplayMode getDisplayMode() {
         return displayMode;
+    }
+
+    public boolean isBottomLineVisible() {
+        return bottomLineVisible;
+    }
+
+    public void setBottomLineVisible(boolean bottomLineVisible) {
+        this.bottomLineVisible = bottomLineVisible;
     }
 
     //endregion
