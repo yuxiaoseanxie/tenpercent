79c6db0647afeedf12e16f27dea89ba3c6cfaf7c
Kevin MacWhinnie <kevin@pinnaplayer.com>
2014-04-11 18:09:35 -0700

    register for notifications with platform (untested)

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/notifications/PushReceiver.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/notifications/PushReceiver.java
@@ -5,15 +5,45 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.android.volley.VolleyError;
+import com.livenation.mobile.android.na.app.ApiServiceBinder;
 import com.livenation.mobile.android.na.app.Constants;
+import com.livenation.mobile.android.na.app.LiveNationApplication;
+import com.livenation.mobile.android.na.helpers.ApiHelper;
 import com.livenation.mobile.android.na.notifications.ui.InboxActivity;
+import com.livenation.mobile.android.platform.api.service.ApiService;
+import com.livenation.mobile.android.platform.api.service.livenation.LiveNationApiService;
+import com.livenation.mobile.android.platform.api.service.livenation.impl.parameter.ApiParameters.RegisterForNotificationsParameters;
 import com.urbanairship.push.PushManager;
+import com.urbanairship.richpush.RichPushManager;
+import com.urbanairship.richpush.RichPushUser;
 
-/**
- * Created by km on 2/27/14.
- */
-public class PushReceiver extends BroadcastReceiver {
+public class PushReceiver extends BroadcastReceiver implements ApiServiceBinder {
     private static final String LOG_TAG = "Live Nation Notifications";
+    private LiveNationApiService apiService;
+    private boolean registeredBeforeApiAttached;
+
+    public PushReceiver() {
+        super();
+
+        getApiHelper().persistentBindApi(this);
+    }
+
+    //region Api Binding
+
+    @Override
+    public void onApiServiceAttached(LiveNationApiService apiService) {
+        Log.i(LOG_TAG, "api service attached");
+        this.apiService = apiService;
+        if(registeredBeforeApiAttached) {
+            Log.i(LOG_TAG, "calling register api after api service attached");
+            registerWithApi();
+        }
+    }
+
+    //endregion
+
+    //region Reception
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -39,7 +69,46 @@ public class PushReceiver extends BroadcastReceiver {
     }
 
     private void registrationFinished(Context context, Intent intent) {
-        /* This is where platform registration will occur. -km */
-        Log.i(LOG_TAG, "Registration finished with APID: " + intent.getStringExtra(PushManager.EXTRA_APID));
+        String apid = intent.getStringExtra(PushManager.EXTRA_APID);
+        Log.i(LOG_TAG, "Registration finished with APID: " + apid);
+
+        if(apiService == null) {
+            registeredBeforeApiAttached = true;
+        } else {
+            registerWithApi();
+        }
     }
+
+    //endregion
+
+
+    //region Registering With Platform
+
+    private void registerWithApi() {
+        String apid = PushManager.shared().getAPID();
+        String userId = RichPushManager.shared().getRichPushUser().getId();
+
+        Log.i(LOG_TAG, "Registering with platform with apid: " + apid + ", UA user id: " + userId);
+
+        RegisterForNotificationsParameters params = new RegisterForNotificationsParameters();
+        params.setDeviceId(apiService.getApiConfig().getDeviceId());
+        params.setTokens(apid, userId);
+        apiService.registerForNotifications(params, new ApiService.BasicApiCallback<Void>() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(LOG_TAG, "Could not register with platform: " + error);
+            }
+
+            @Override
+            public void onResponse(Void response) {
+                Log.i(LOG_TAG, "Completed platform registration");
+            }
+        });
+    }
+
+    private ApiHelper getApiHelper() {
+        return LiveNationApplication.get().getApiHelper();
+    }
+
+    //endregion
 }
