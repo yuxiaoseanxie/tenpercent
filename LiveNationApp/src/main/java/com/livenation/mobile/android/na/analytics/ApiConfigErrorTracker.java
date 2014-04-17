c4a7c6369b21772ec7e0cf276f2b1a2cfccb9701
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-04-17 16:05:12 -0700

    Add class implementing Error Tracker interface

---
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/analytics/ApiConfigErrorTracker.java
@@ -0,0 +1,16 @@
+package com.livenation.mobile.android.na.analytics;
+
+import com.livenation.mobile.android.platform.api.service.livenation.analytics.ErrorTracker;
+
+import java.util.Map;
+
+/**
+ * Created by elodieferrais on 4/17/14.
+ */
+public class ApiConfigErrorTracker implements ErrorTracker {
+
+    @Override
+    public void track(String logTitle, Map<String, String> properties) {
+        
+    }
+}
