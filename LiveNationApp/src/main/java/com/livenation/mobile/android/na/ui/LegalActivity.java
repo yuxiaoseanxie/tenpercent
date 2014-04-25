b1f5d289adc103344bac447861961caf76b79786
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-04-25 16:45:59 -0700

    add activity + layout for legal section

---
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/LegalActivity.java
@@ -0,0 +1,13 @@
+package com.livenation.mobile.android.na.ui;
+
+import android.os.Bundle;
+
+/**
+ * Created by elodieferrais on 4/25/14.
+ */
+public class LegalActivity extends LiveNationFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
