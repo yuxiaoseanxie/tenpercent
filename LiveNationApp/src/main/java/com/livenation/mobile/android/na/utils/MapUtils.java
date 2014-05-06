ff85957f63075e7174c2b612062a558ff2c20b4c
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-05-06 15:51:07 -0700

    fix bug for google map v7

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/MapUtils.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/MapUtils.java
@@ -1,5 +1,6 @@
 package com.livenation.mobile.android.na.utils;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,10 +14,11 @@ import java.util.Locale;
  * Created by elodieferrais on 4/24/14.
  */
 public class MapUtils {
-    static public void redirectToMapApplication(double lat, double lng, Context context) {
+    static public void redirectToMapApplication(double lat, double lng, String name,  Context context) {
 
-        String uri = String.format(Locale.ENGLISH, "geo:%f,%f", lat, lng);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("geo:"+lat+","+lng+"?q="+lat+","+lng+"("+name+")"));
+        intent.setComponent(new ComponentName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity"));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         final PackageManager pm = context.getPackageManager();
