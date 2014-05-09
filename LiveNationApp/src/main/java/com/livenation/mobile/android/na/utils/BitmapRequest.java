09536a9cabb0e48891a14ce17d95dfb6a617e27c
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-05-09 13:37:13 -0700

    close the input stream

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/BitmapRequest.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/BitmapRequest.java
@@ -6,9 +6,11 @@ import android.graphics.BitmapFactory;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -26,9 +28,14 @@ public class BitmapRequest extends Request<Bitmap> {
 
     @Override
     protected Response<Bitmap> parseNetworkResponse(NetworkResponse response) {
-        InputStream stream = new ByteArrayInputStream(response.data);
-        Bitmap img = BitmapFactory.decodeStream(stream);
-        return Response.success(img, HttpHeaderParser.parseCacheHeaders(response));
+        try {
+            InputStream stream = new ByteArrayInputStream(response.data);
+            Bitmap img = BitmapFactory.decodeStream(stream);
+            stream.close();
+            return Response.success(img, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (IOException e) {
+            return Response.error(new VolleyError(e));
+        }
     }
 
     @Override
