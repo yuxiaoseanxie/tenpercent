f05b7be92ccaa4eff27b718ed2568d167f6aadb9
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-05-15 14:42:35 -0700

    add text color and redirect to favorites screen when click on the button

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/notifications/ui/BaseInboxFragment.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/notifications/ui/BaseInboxFragment.java
@@ -5,14 +5,17 @@
 package com.livenation.mobile.android.na.notifications.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.livenation.mobile.android.na.R;
+import com.livenation.mobile.android.na.ui.FavoriteActivity;
 import com.urbanairship.richpush.RichPushMessage;
 
 import java.util.ArrayList;
@@ -21,7 +24,7 @@ import java.util.List;
 /**
  * A list fragment that shows rich push messages.
  */
-public abstract class BaseInboxFragment extends ListFragment {
+public abstract class BaseInboxFragment extends ListFragment implements View.OnClickListener{
     private OnMessageListener listener;
     private RichPushMessageAdapter adapter;
     private List<String> selectedMessageIds = new ArrayList<String>();
@@ -52,12 +55,22 @@ public abstract class BaseInboxFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
         View view = LayoutInflater.from(getActivity().getApplicationContext()).inflate(R.layout.fragment_inbox_list_empty_view, null, false);
         ((ViewGroup)getListView().getParent()).addView(view);
+
+        Button favoriteButton = (Button) view.findViewById(R.id.notif_no_notification_favorite_button);
+        favoriteButton.setOnClickListener(this);
+
         getListView().setEmptyView(view);
     }
 
     @Override
     public void onListItemClick(ListView list, View view, int position, long id) {
         this.listener.onMessageOpen(this.adapter.getItem(position));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent favoriteIntent = new Intent(v.getContext(), FavoriteActivity.class);
+        startActivity(favoriteIntent);
     }
 
     /**
