e066f716a568a51154803f68a3972153b5571d9a
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-05-28 14:36:06 -0700

    Add credits + fix rotation

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/LegalActivity.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/LegalActivity.java
@@ -14,43 +14,61 @@ import com.livenation.mobile.android.na.analytics.AnalyticsCategory;
 import com.livenation.mobile.android.na.analytics.LiveNationAnalytics;
 import com.livenation.mobile.android.na.ui.fragments.WebViewFragment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by elodieferrais on 4/25/14.
  */
 public class LegalActivity extends LiveNationFragmentActivity {
     private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-    private static final String TERMS_OF_USE_FRAGMENT_TAG = "terms_of_use_fragment_tab";
-    private static final String PRIVACY_POLICY_FRAGMENT_TAG = "privacy_policy_tab";
+    private static final String TERMS_OF_USE_FRAGMENT_TAB = "terms_of_use_fragment_tab";
+    private static final String PRIVACY_POLICY_FRAGMENT_TAB = "privacy_policy_tab";
+    private static final String CREDITS_FRAGMENT_TAB = "credits_tab";
+    private String[] tagsByIndex = new String[] {TERMS_OF_USE_FRAGMENT_TAB, PRIVACY_POLICY_FRAGMENT_TAB, CREDITS_FRAGMENT_TAB};
+    private List<Fragment> fragmentsByIndex = new ArrayList<Fragment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legal);
 
+        fragmentsByIndex.clear();
 
-        ActionBar.Tab termOfUseTab, privacyPolicyTab;
-        WebViewFragment termOfUseTabFragment = (WebViewFragment) getFragmentManager().findFragmentByTag(TERMS_OF_USE_FRAGMENT_TAG);
-        WebViewFragment privacyPolicyTabFragment = (WebViewFragment) getFragmentManager().findFragmentByTag(PRIVACY_POLICY_FRAGMENT_TAG);
+        ActionBar.Tab termOfUseTab, privacyPolicyTab, creditsTab;
+        WebViewFragment termOfUseTabFragment = (WebViewFragment) getFragmentManager().findFragmentByTag(TERMS_OF_USE_FRAGMENT_TAB);
+        WebViewFragment privacyPolicyTabFragment = (WebViewFragment) getFragmentManager().findFragmentByTag(PRIVACY_POLICY_FRAGMENT_TAB);
+        WebViewFragment creditsTabFragment = (WebViewFragment) getFragmentManager().findFragmentByTag(CREDITS_FRAGMENT_TAB);
 
         if (termOfUseTabFragment == null) {
             termOfUseTabFragment = WebViewFragment.newInstance(getString(R.string.legal_terms_of_use_url));
         }
-
+        fragmentsByIndex.add(termOfUseTabFragment);
         if (privacyPolicyTabFragment == null) {
             privacyPolicyTabFragment = WebViewFragment.newInstance(getString(R.string.legal_privacy_policy_url));
         }
-
+        fragmentsByIndex.add(privacyPolicyTabFragment);
+        if(creditsTabFragment == null) {
+            creditsTabFragment = WebViewFragment.newInstance(getString(R.string.credits_url));
+        }
+        fragmentsByIndex.add(creditsTabFragment);
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         termOfUseTab = actionBar.newTab().setText(getString(R.string.legal_terms_of_use));
         privacyPolicyTab = actionBar.newTab().setText(getString(R.string.legal_privacy_policy));
+        creditsTab = actionBar.newTab().setText(getString(R.string.legal_credits));
 
-        termOfUseTab.setTabListener(new WebViewTabListener(termOfUseTabFragment, TERMS_OF_USE_FRAGMENT_TAG));
-        privacyPolicyTab.setTabListener(new WebViewTabListener(privacyPolicyTabFragment, PRIVACY_POLICY_FRAGMENT_TAG));
+        termOfUseTab.setTabListener(new WebViewTabListener(termOfUseTabFragment, TERMS_OF_USE_FRAGMENT_TAB));
+        privacyPolicyTab.setTabListener(new WebViewTabListener(privacyPolicyTabFragment, PRIVACY_POLICY_FRAGMENT_TAB));
+        creditsTab.setTabListener(new WebViewTabListener(creditsTabFragment, CREDITS_FRAGMENT_TAB));
+
 
         actionBar.addTab(termOfUseTab);
         actionBar.addTab(privacyPolicyTab);
+        actionBar.addTab(creditsTab);
     }
 
     @Override
@@ -58,7 +76,23 @@ public class LegalActivity extends LiveNationFragmentActivity {
         super.onRestoreInstanceState(savedInstanceState);
         // Restore the previously serialized current tab position.
         if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-            getActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+            int index = savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM);
+            getActionBar().setSelectedNavigationItem(index);
+            int j;
+            for (j = 0; j < fragmentsByIndex.size() ; j++) {
+                Fragment fragment = fragmentsByIndex.get(j);
+               if (j == index) {
+                   if (!fragment.isAdded()) {
+                       getFragmentManager().beginTransaction().add(R.id.activity_legal_container, fragment, tagsByIndex[j]).commit();
+                   } else {
+                       getFragmentManager().beginTransaction().show(fragment).commit();
+                   }
+               } else {
+                   if (fragment.isAdded()) {
+                       getFragmentManager().beginTransaction().hide(fragment).commit();
+                   }
+               }
+            }
         }
     }
 
@@ -87,7 +121,7 @@ public class LegalActivity extends LiveNationFragmentActivity {
             } else {
                 getFragmentManager().beginTransaction().show(fragment).commit();
             }
-            if (TERMS_OF_USE_FRAGMENT_TAG.equals(tag)) {
+            if (TERMS_OF_USE_FRAGMENT_TAB.equals(tag)) {
                 LiveNationAnalytics.track(AnalyticConstants.TERMS_OF_USE_TAP, AnalyticsCategory.LEGAL);
             } else {
                 LiveNationAnalytics.track(AnalyticConstants.PRIVACY_POLICY_TAP, AnalyticsCategory.LEGAL);
@@ -97,7 +131,9 @@ public class LegalActivity extends LiveNationFragmentActivity {
 
         @Override
         public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            getFragmentManager().beginTransaction().hide(fragment).commit();
+            if (fragment.isAdded()) {
+                getFragmentManager().beginTransaction().hide(fragment).commit();
+            }
         }
 
         @Override
--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/fragments/WebViewFragment.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/fragments/WebViewFragment.java
@@ -2,12 +2,16 @@ package com.livenation.mobile.android.na.ui.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import com.livenation.mobile.android.na.R;
 
@@ -27,27 +31,41 @@ public class WebViewFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        FrameLayout relativeLayout = new FrameLayout(getActivity());
+        final ProgressBar progressBar = new ProgressBar(getActivity());
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        progressBar.setLayoutParams(params);
         webView = new WebView(getActivity());
-        if (savedInstanceState == null) {
-            WebSettings webSettings = webView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webView.setWebViewClient(new WebViewClient());
+        relativeLayout.addView(webView);
+        relativeLayout.addView(progressBar);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
 
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+            }
+        });
+
+
+        if (savedInstanceState == null) {
             String url = getArguments().getString(ARG_URL);
             webView.loadUrl(url);
         } else {
             webView.restoreState(savedInstanceState);
         }
 
-        return webView;
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        return relativeLayout;
     }
 
     @Override
