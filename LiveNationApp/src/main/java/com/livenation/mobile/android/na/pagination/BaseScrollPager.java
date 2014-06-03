15e2a1a02571498453bc577f16e4b55162bd3514
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-06-03 15:04:37 -0700

    Assume that there is an empty view in the layout of the home screen tabs

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/pagination/BaseScrollPager.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/pagination/BaseScrollPager.java
@@ -52,7 +52,7 @@ public abstract class BaseScrollPager<TItemType extends IdEquals<TItemType>> imp
     }
 
     public void load() {
-        if (paginatedFetcher != null || !hasMorePages) {
+        if (paginatedFetcher != null) {
             return;
         }
 
