e9c41af6051be70a909368f2b4636330a91c81fa
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2014-08-12 15:33:09 -0700

    blow away proof of concept home activity test + modernize send library test

---
+++ LiveNationApp/src/androidTest/java/mock/EnvironmentProviderMock.java
@@ -0,0 +1,14 @@
+package mock;
+
+import com.livenation.mobile.android.platform.init.Environment;
+import com.livenation.mobile.android.platform.init.provider.EnvironmentProvider;
+
+/**
+ * Created by elodieferrais on 7/22/14.
+ */
+public class EnvironmentProviderMock implements EnvironmentProvider {
+    @Override
+    public Environment getEnvironment() {
+        return Environment.Staging;
+    }
+}
