919f815a06bdcf26cb9275c2d096682260f60191
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2014-08-13 14:46:57 -0700

    jackson tests pass

--- LiveNationApp/src/androidTest/java/stubs/StubTests.java
+++ LiveNationApp/src/androidTest/java/stubs/StubTests.java
@@ -1,16 +1,26 @@
 package stubs;
 
 import android.test.InstrumentationTestCase;
+import android.widget.Adapter;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.io.HttpResponseParser;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Collections;
+import stubs.converters.JacksonJsonConverter;
 
 import static stubs.StubHttpStack.createBasicHeaders;
 import static stubs.StubHttpStack.emptyHeaders;
@@ -18,6 +28,7 @@ import static stubs.StubHttpStack.emptyHeaders;
 public class StubTests extends InstrumentationTestCase {
     private static final String TEST_URL = "http://example.com/test.file";
     private static final String TEST_BODY = "hello, world!";
+    private static final JacksonJsonConverter JACKSON_JSON_CONVERTER = new JacksonJsonConverter(new ObjectMapper());
 
     private final StubHttpStack stack = new StubHttpStack();
     private final RequestQueue queue = stack.getRequestQueue();
@@ -41,7 +52,7 @@ public class StubTests extends InstrumentationTestCase {
         assertEquals(TEST_BODY, response);
     }
 
-    public void testSuccessfulJsonStub() {
+    public void testSuccessfulGetJsonObjectStub() {
         JSONObject stubbedResponse = new JSONObject();
         try {
             stubbedResponse.put("worked", true);
@@ -56,5 +67,21 @@ public class StubTests extends InstrumentationTestCase {
 
         JSONObject response = adapter.getOrFail();
         assertEquals(stubbedResponse.toString(), response.toString());
+    }
+
+    public void testSuccessfulGetJacksonConverterStub() {
+        stack.stubGet(TEST_URL, emptyHeaders())
+             .andReturnJson(new TestObject(), JACKSON_JSON_CONVERTER, emptyHeaders(), 200);
+
+        SyncResponseAdapter<JSONObject> adapter = new SyncResponseAdapter<JSONObject>();
+        queue.add(new JsonObjectRequest(Request.Method.GET, TEST_URL, null, adapter, adapter));
+
+        JSONObject response = adapter.getOrFail();
+        assertTrue(response.optBoolean("worked"));
+    }
+
+
+    private static class TestObject {
+        @JsonProperty("worked") boolean worked = true;
     }
 }
