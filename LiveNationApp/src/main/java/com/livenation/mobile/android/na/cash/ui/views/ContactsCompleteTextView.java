742cc03892d57868e6c050c0ed278f7b76fb1a78
Charlie Chilton <charliec@livenationlabs.com>
2014-08-15 13:28:04 -0700

    Perform input throttling on contact lookup autocomplete

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/cash/ui/views/ContactsCompleteTextView.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/cash/ui/views/ContactsCompleteTextView.java
@@ -1,6 +1,10 @@
 package com.livenation.mobile.android.na.cash.ui.views;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -8,6 +12,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.livenation.mobile.android.na.R;
+import com.livenation.mobile.android.na.app.Constants;
 import com.livenation.mobile.android.na.cash.model.ContactData;
 import com.livenation.mobile.android.na.cash.model.ContactsCursorAdapter;
 
@@ -47,4 +52,51 @@ public class ContactsCompleteTextView extends TokenCompleteTextView<ContactData>
             return new ContactData("{}", completionText, null, completionText, null);
         }
     }
+
+    //region throttler
+
+    /**
+     * Throttle keypresses that occur in quick succession so that we reduce the amount of intense
+     * DB hits we have to perform
+     */
+    private static final String KEY_TEXT = "text";
+    private static final String KEY_START = "start";
+    private static final String KEY_END = "end";
+    private static final String KEY_KEY_CODE = "keycode";
+
+    private Handler throttler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            String text = msg.getData().getString(KEY_TEXT);
+            int start = msg.getData().getInt(KEY_START);
+            int end = msg.getData().getInt(KEY_END);
+            int keyCode = msg.getData().getInt(KEY_KEY_CODE);
+
+            ContactsCompleteTextView.super.performFiltering(text, start, end, keyCode);
+        }
+    };
+
+    @Override
+    protected void performFiltering(@NonNull CharSequence text, int start, int end, int keyCode) {
+        //the cast toString() intentionally creates a copy of the value of the text var, that
+        //won't get mutated during the TEXT_CHANGED_POST_DELAY window
+        Message message = getMessage(text.toString(), start, end, keyCode);
+        throttler.removeMessages(0);
+        throttler.sendMessageDelayed(message, Constants.TEXT_CHANGED_POST_DELAY);
+    }
+
+    private Message getMessage(String text, int start, int end, int keyCode) {
+        Message message = new Message();
+        Bundle data = new Bundle();
+
+        data.putString(KEY_TEXT, text);
+        data.putInt(KEY_START, start);
+        data.putInt(KEY_END, end);
+        data.putInt(KEY_KEY_CODE, keyCode);
+
+        message.setData(data);
+        return message;
+    }
+
+    //endregion
 }
