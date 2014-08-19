dbe5dc0f1e904597db1024327d9771ac50f65549
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2014-08-19 15:01:14 -0700

    expunge obsolete test

--- LiveNationApp/src/androidTest/java/square/SquareCashServiceTests.java
+++ LiveNationApp/src/androidTest/java/square/SquareCashServiceTests.java
@@ -13,6 +13,7 @@ import com.livenation.mobile.android.na.cash.service.responses.CashSession;
 import org.json.JSONObject;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
 
 import stubs.StubHttpStack;
@@ -24,22 +25,14 @@ import static stubs.StubHttpStack.createHeadersWithContentType;
 public class SquareCashServiceTests extends InstrumentationTestCase {
     private final StubHttpStack stack = new StubHttpStack();
     private final MockSessionPersistenceProvider persistenceProvider = new MockSessionPersistenceProvider();
-    private final SquareCashService.CustomerIdProvider customerIdProvider = new SquareCashService.CustomerIdProvider() {
-        @Override
-        public void provideSquareCustomerId(Response.Listener<String> onResponse) {
-            onResponse.onResponse("this-is-a-test-session");
-        }
-    };
     private SquareCashService service;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
-        persistenceProvider.populate();
         this.service = new SquareCashService(getInstrumentation().getTargetContext(),
                                              stack.getRequestQueue(),
-                                             customerIdProvider,
                                              persistenceProvider);
     }
 
@@ -62,15 +55,10 @@ public class SquareCashServiceTests extends InstrumentationTestCase {
     private static class MockSessionPersistenceProvider implements SessionPersistenceProvider {
         private CashSession cashSession = new CashSession();
 
-        public void reset() {
-            this.cashSession = new CashSession();
-        }
-
-        public void populate() {
+        public MockSessionPersistenceProvider() {
             cashSession.setAccessToken("123thisisfake");
             cashSession.setCustomerId("this-is-a-test-session");
-            cashSession.setExpiresAt("2020-01-01T00:00:00Z");
-            cashSession.setTokenType(CashSession.TOKEN_TYPE_BEARER);
+            cashSession.setExpiresAt(new Date());
         }
 
         @Override
@@ -86,24 +74,6 @@ public class SquareCashServiceTests extends InstrumentationTestCase {
 
     //endregion
 
-
-    public void testSessionCreation() throws Exception {
-        persistenceProvider.reset();
-
-        JSONObject body = new JSONObject("{\"customer_id\":\"this-is-a-test-session\",\"phone_number\":\"1234567890\",\"client_id\":\"a2jqttf932pokmmkp0xtzz8ku\",\"response_type\":\"token\"}");
-        JSONObject response = new JSONObject("{\"access_token\": \"123thisisfake\", \"customer_id\": \"this-is-a-test-session\", \"expires_at\": \"2020-01-01T00:00:00Z\", \"token_type\": \"bearer\"}");
-        stack.stubPost(service.makeUrl("oauth2/authorize/cash", null), createHeaders(), body)
-             .andReturnJson(response, createEmptyHeaders(), 200);
-
-        SyncResponseAdapter<CashSession> adapter = new SyncResponseAdapter<CashSession>();
-        service.startSession(null, "1234567890", adapter);
-
-        CashSession session = adapter.getOrFail();
-        assertEquals("123thisisfake", session.getAccessToken());
-        assertEquals("2020-01-01T00:00:00Z", session.getExpiresAt());
-        assertEquals("this-is-a-test-session", session.getCustomerId());
-        assertEquals(CashSession.TOKEN_TYPE_BEARER, session.getTokenType());
-    }
 
     public void testCustomerStatus() throws Exception {
         JSONObject response = new JSONObject("{\"payments\":[],\"blockers\":{\"url\":\"https://cash.square-sandbox.com/cash/enroll/c7zay6myrvqabdbgb0hfcb5i9\",\"card\":{},\"phone_number\":{}},\"passcode_confirmation_enabled\":false}");
