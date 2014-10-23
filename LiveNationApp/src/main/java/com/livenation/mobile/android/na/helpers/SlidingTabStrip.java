4eb3f9ee25c79bfd310bd4a4b56c4aef2bd93070
Elodie Ferrais <elodie.ferrais@gmail.com>
2014-10-23 10:28:44 +0200

    fix CommitPrefEdits and SuspiciousImport error

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/SlidingTabStrip.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/helpers/SlidingTabStrip.java
@@ -16,7 +16,6 @@
 
 package com.livenation.mobile.android.na.helpers;
 
-import android.R;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -63,7 +62,7 @@ class SlidingTabStrip extends LinearLayout {
         final float density = getResources().getDisplayMetrics().density;
 
         TypedValue outValue = new TypedValue();
-        context.getTheme().resolveAttribute(R.attr.colorForeground, outValue, true);
+        context.getTheme().resolveAttribute(android.R.attr.colorForeground, outValue, true);
         final int themeForegroundColor = outValue.data;
 
         mDefaultBottomBorderColor = setColorAlpha(themeForegroundColor,
