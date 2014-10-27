8bb5a3717f8729aef839551984d40bdfc9ba3f7c
Elodie Ferrais <elodie.ferrais@gmail.com>
2014-10-27 16:12:00 +0100

    add boolean to know if its new user or an update

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/EventUtils.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/EventUtils.java
@@ -1,10 +1,13 @@
 package com.livenation.mobile.android.na.utils;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.livenation.mobile.android.na.app.Constants;
 import com.livenation.mobile.android.na.app.LiveNationApplication;
 import com.livenation.mobile.android.na.ui.OrderConfirmationActivity;
 import com.livenation.mobile.android.na.ui.ShowActivity;
@@ -19,19 +22,24 @@ import java.util.List;
  * Created by elodieferrais on 10/20/14.
  */
 public class EventUtils {
-    static public boolean isSDPAvoidable(Event event) {
+    static public boolean isSDPAvoidable(Event event, Context context) {
         List<TicketOffering> offerings = event.getTicketOfferings();
+        final SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.SharedPreferences.AB_TESTING, Context.MODE_PRIVATE);
+        boolean isNewUser = sharedPreferences.getBoolean(Constants.SharedPreferences.IS_NEW_USER, true);
+
         return (event.getOnSaleDate()!= null
                 && event.getOnSaleDate().compareTo(Calendar.getInstance().getTime()) <= 0
                 && offerings.size() == 1
                 && Ticketing.isTicketmasterUrl(offerings.get(0).getPurchaseUrl())
-                && LiveNationApplication.get().getInstalledAppConfig().isCommerceAvailable());
+                && LiveNationApplication.get().getInstalledAppConfig().isCommerceAvailable()
+                && isNewUser
+                );
     }
 
     static public void redirectToSDPOrEDP(Event event, final FragmentActivity activity) {
         Intent intent = new Intent(activity, ShowActivity.class);
         List<TicketOffering> offerings = event.getTicketOfferings();
-        if (EventUtils.isSDPAvoidable(event)) {
+        if (EventUtils.isSDPAvoidable(event, activity)) {
             Intent confirmIntent = new Intent(activity, OrderConfirmationActivity.class);
             confirmIntent.putExtra(OrderConfirmationActivity.EXTRA_EVENT, event);
             confirmIntent.putExtra(com.livenation.mobile.android.ticketing.analytics.AnalyticConstants.PROP_IS_SDP_SHOWN, true);
