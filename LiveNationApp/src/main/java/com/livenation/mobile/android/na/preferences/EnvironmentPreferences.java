56ac162cbe8257427a2d83a339a238b9732ad61c
Elodie Ferrais <elodie.ferrais@gmail.com>
2014-11-13 11:45:56 +0100

    update read me. Add documentation for running test and lint

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/preferences/EnvironmentPreferences.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/preferences/EnvironmentPreferences.java
@@ -2,6 +2,7 @@ package com.livenation.mobile.android.na.preferences;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.v4.BuildConfig;
 import android.util.Log;
 
 import com.livenation.mobile.android.na.app.Constants;
@@ -23,13 +24,22 @@ public class EnvironmentPreferences {
 
     public Environment getConfiguredEnvironment() {
         String environmentSerialize = sharedPreferences.getString(Constants.SharedPreferences.ENVIRONMENT, null);
-        Environment env = Environment.Production;
+        Environment env;
+        if (BuildConfig.DEBUG) {
+            Log.d("Elodie", "env debug");
+            env = Environment.Staging;
+        } else {
+            Log.d("Elodie", "env prod");
+            env = Environment.Production;
+        }
         if (environmentSerialize != null && !environmentSerialize.isEmpty()) {
             try {
                 env = (Environment) SerializableHelper.fromString(environmentSerialize);
             } catch (IOException e) {
+                Log.d("Elodie", "IOException e");
                 return Environment.Production;
             } catch (ClassNotFoundException e) {
+                Log.d("Elodie", "ClassNotFoundException e");
                 return Environment.Production;
             }
         }
