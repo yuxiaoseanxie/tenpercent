c0b6bee4b7f94782de46a35967e3a1efbecba3cc
Charlie Chilton <charliec@livenationlabs.com>
2014-11-18 12:15:15 -0800

    Observable/RX based infrastructure tests working for Uber API

--- Uber/src/androidTest/java/com/livenation/mobile/android/na/uber/ApplicationTest.java
+++ Uber/src/androidTest/java/com/livenation/mobile/android/na/uber/ApplicationTest.java
@@ -5,8 +5,19 @@ import android.test.ApplicationTestCase;
 
 import com.livenation.mobile.android.na.uber.service.UberHelper;
 import com.livenation.mobile.android.na.uber.service.UberService;
-import com.livenation.mobile.android.na.uber.service.model.UberPrices;
-import com.livenation.mobile.android.na.uber.service.model.UberProducts;
+import com.livenation.mobile.android.na.uber.service.model.UberPriceResponse;
+import com.livenation.mobile.android.na.uber.service.model.UberProductResponse;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -27,15 +38,23 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     }
 
     public void testGetProducts() {
-        UberProducts products = uberService.getProducts(LOCATION_SF[0], LOCATION_SF[1]);
-        assertNotNull(products);
-        assertTrue(products.getProducts().size() > 0);
+        Observable<UberProductResponse> products = uberService.getProducts(LOCATION_SF[0], LOCATION_SF[1]);
+        TestSubscriber<UberProductResponse> testSubscriber = new TestSubscriber<UberProductResponse>();
+        products.subscribe(testSubscriber);
+        testSubscriber.awaitTerminalEvent(10, TimeUnit.SECONDS);
+        List<UberProductResponse> completed = testSubscriber.getOnNextEvents();
+        assertTrue(completed.size() > 0);
+        assertTrue(completed.get(0).getProducts().size() > 0);
     }
 
     public void testGetEstimates() {
-        UberPrices prices = uberService.getEstimates(LOCATION_SF[0], LOCATION_SF[1], LOCATION_EAST_BAY[0], LOCATION_EAST_BAY[1]);
-        assertNotNull(prices);
-        assertTrue(prices.getPrices().size() > 0);
+        Observable<UberPriceResponse> prices = uberService.getEstimates(LOCATION_SF[0], LOCATION_SF[1], LOCATION_EAST_BAY[0], LOCATION_EAST_BAY[1]);
+        TestSubscriber<UberPriceResponse> testSubscriber = new TestSubscriber<UberPriceResponse>();
+        prices.subscribe(testSubscriber);
+        testSubscriber.awaitTerminalEvent(10, TimeUnit.SECONDS);
+        List<UberPriceResponse> completed = testSubscriber.getOnNextEvents();
+        assertTrue(completed.size() > 0);
+        assertTrue(completed.get(0).getPrices().size() > 0);
     }
 }
 
