0f4bd520c370968679345f7d9833a22a398915c0
Charlie Chilton <charliec@livenationlabs.com>
2015-01-27 10:55:21 -0800

    Fix for analytic properties appearing correctly in Crashyltics. [Refs #87032270]

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/analytics/Props.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/analytics/Props.java
@@ -1,5 +1,7 @@
 package com.livenation.mobile.android.na.analytics;
 
+import com.google.gson.Gson;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -8,6 +10,8 @@ import java.util.Set;
  * Created by cchilton on 1/12/15.
  */
 public class Props {
+    private static final Gson gson = new Gson();
+
     private final Map<String, Object> data = new HashMap<>();
 
     public void put(String key, Object value) {
@@ -20,5 +24,10 @@ public class Props {
 
     public Set<String> keySet() {
         return data.keySet();
+    }
+
+    @Override
+    public String toString() {
+        return gson.toJson(data);
     }
 }
