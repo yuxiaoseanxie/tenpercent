f7ba1330417b3f6806241e217669f1c87a2d91d3
Elodie Ferrais <elodie.ferrais@gmail.com>
2015-02-10 14:32:43 -0800

    remove unused imports
    [#87859776]

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/uber/dialogs/UberDialogFragment.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/uber/dialogs/UberDialogFragment.java
@@ -21,7 +21,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.InsetDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
