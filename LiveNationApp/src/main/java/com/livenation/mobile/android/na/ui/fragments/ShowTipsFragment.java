a4e70454bda4d6c4f71c9a6f0af2efcec0fe534f
Elodie Ferrais <elodie.ferrais@gmail.com>
2015-02-24 16:13:52 -0800

    add showtips fragment with a lineup fragment inside
    [#88935100]

---
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/ui/fragments/ShowTipsFragment.java
@@ -0,0 +1,30 @@
+package com.livenation.mobile.android.na.ui.fragments;
+
+import com.livenation.mobile.android.na.R;
+import com.livenation.mobile.android.platform.api.service.livenation.impl.model.Artist;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by elodieferrais on 2/23/15.
+ */
+public class ShowTipsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_showtips, container,false);
+
+        LineUpFragment lineUpFragment = new LineUpFragment();
+        lineUpFragment.setArtists(new ArrayList<Artist>());
+        getChildFragmentManager().beginTransaction().add(R.id.lineup_container, lineUpFragment).commit();
+
+        return view;
+    }
+}
