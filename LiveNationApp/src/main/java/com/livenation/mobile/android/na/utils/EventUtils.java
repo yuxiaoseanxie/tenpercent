9e993a6066bad0f6c6e3afa0df6ccc40ae056ca3
Elodie Ferrais <elodie.ferrais@gmail.com>
2015-03-02 18:48:13 -0800

    implement isAcomingEvent() + test
    [#89063846]

--- LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/EventUtils.java
+++ LiveNationApp/src/main/java/com/livenation/mobile/android/na/utils/EventUtils.java
@@ -1,25 +1,16 @@
 package com.livenation.mobile.android.na.utils;
 
 
-import com.livenation.mobile.android.na.app.Constants;
-import com.livenation.mobile.android.na.app.LiveNationApplication;
-import com.livenation.mobile.android.na.providers.ConfigFileProvider;
-import com.livenation.mobile.android.na.ui.OrderConfirmationActivity;
 import com.livenation.mobile.android.na.ui.ShowActivity;
-import com.livenation.mobile.android.platform.api.service.livenation.impl.BasicApiCallback;
 import com.livenation.mobile.android.platform.api.service.livenation.impl.model.Event;
-import com.livenation.mobile.android.platform.api.service.livenation.impl.model.TicketOffering;
-import com.livenation.mobile.android.platform.api.transport.error.LiveNationError;
-import com.livenation.mobile.android.ticketing.Ticketing;
 
 import java.util.Calendar;
-import java.util.List;
+import java.util.Date;
+import java.util.Locale;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by elodieferrais on 10/20/14.
@@ -38,5 +29,17 @@ public class EventUtils {
         Intent intent = new Intent(context, ShowActivity.class);
         intent.putExtras(ShowActivity.getArguments(eventId));
         context.startActivity(intent);
+    }
+
+    static public boolean isAcomingEvent(Event event) {
+        Date startDate = event.getLocalStartTime();
+        if (startDate != null) {
+            long timeDiff = startDate.getTime() - Calendar.getInstance(Locale.getDefault()).getTimeInMillis();
+            if (Math.abs(timeDiff) <= (12 * 60 * 60 * 1000)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
